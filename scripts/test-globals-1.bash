8c224e6d1f2f2f3b9b292dc5509e83f734e209a0
Stefaan Coussement <543534+stefaanc@users.noreply.github.com>
2019-07-30 15:27:54 +0100

    some extra test-cases

--- scripts/test-globals-1.bash
+++ scripts/test-globals-1.bash
@@ -2,11 +2,15 @@
 
 . "$( dirname ${BASH_SOURCE[0]} )/.globals.bash"
 
+#trap 'LASTEXITCODE=$?; echo "trapped error"; exit $LASTEXITCODE' ERR
+#trap 'LASTEXITCODE=$?; echo "trapped exit"; exit $LASTEXITCODE' EXIT
+
 echo "### test-globals-1.bash"
 
 echo "### \$GLOBALS_INJECT=$GLOBALS_INJECT"
 echo ""
 
+#call "$( dirname ${BASH_SOURCE[0]} )/test-globals-2.bash-xxx" my-param
 call "$( dirname ${BASH_SOURCE[0]} )/test-globals-2.bash" my-param
 echo "\$?=$?"
 echo ""
